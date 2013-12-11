2ea890e64221a1f6863a14aff9a8a82203fd6370
Remi Barraquand <dev@remibarraquand.com>
2013-12-11 21:16:51 +0100

    cleaning up post helper

--- lib/helpers/post.rb
+++ lib/helpers/post.rb
@@ -1,11 +1,16 @@
+# encoding: utf-8
+
 module Nanoc::Helpers
-    module Post
-        def get_pretty_date(post)
-            if post[:created_at]
-                Date.parse(post[:created_at].to_s).strftime('%B %-d, %Y')
-            else
-                ""
-            end
-        end
+
+  # Contains functions for posting.
+  module Post
+    
+    def get_pretty_date(post)
+      if post[:created_at]
+        Date.parse(post[:created_at].to_s).strftime('%B %-d, %Y')
+      else
+        ""
+      end
     end
+  end
 end
