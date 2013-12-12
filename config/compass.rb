b1b5fafcaeb780881b04c208d6701f5961fcbd7b
Remi Barraquand <dev@remibarraquand.com>
2013-12-12 22:17:02 +0100

    fixing bugs in routing path. Adding fonts to compass

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,11 @@
 http_path    = "/" 
 project_path = File.expand_path('.')
 
-images_dir = "content/media/images/"
-generated_images_dir = "output/media/images"
+sass_dir     = 'content/assets/stylesheets'
+css_dir      = 'output/assets/stylesheets'
+
+images_dir = 'content/media/images/'
+generated_images_dir = 'output/media/images'
+
+fonts_dir = 'content/assets/fonts'
+http_fonts_path = 'assets/fonts'
--- lib/helpers/layout.rb
+++ lib/helpers/layout.rb
@@ -9,10 +9,7 @@ module Nanoc::Helpers
     include Nanoc::Helpers::HTMLEscape
 
     def page_stylesheet_tag(media: :all)
-      puts "checking for #{@item.identifier}"
-
-      css_identifier = File.join('/assets/css/pages', @item.identifier).to_s
-
+      css_identifier = File.join('/assets/stylesheets/pages', @item.identifier).to_s
       if css_item = @items.at(css_identifier)
         "<link href='#{h css_item.path}' media='#{media}' rel='stylesheet'>"
       end
